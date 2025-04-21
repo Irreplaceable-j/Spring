e5759a88ae0b642bf0b97ac5b9a61ccd341041fe
Kimoon J <jkm3093@naver.com>
2025-04-21 23:10:15 +0900

    (0421) test

--- springboot-jpa/src/test/java/com/grepp/spring/test/BookTest.java
+++ springboot-jpa/src/test/java/com/grepp/spring/test/BookTest.java
@@ -1,6 +1,7 @@
 package com.grepp.spring.test;
 
 import com.grepp.spring.app.model.book.code.Category;
+import com.grepp.spring.test.book.BookTestRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
