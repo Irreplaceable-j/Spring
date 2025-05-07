6e1e5a37dd9f4ba7936e13f4104112aa40c61375
Kimoon J <jkm3093@naver.com>
2025-05-07 23:48:45 +0900

    (0425) 임베딩 test

---
+++ springboot-boilerplate/src/test/java/com/grepp/spring/app/model/student/repository/StudentEmbeddingRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.grepp.spring.app.model.student.repository;
+
+import com.grepp.spring.app.model.student.code.MBTI;
+import com.grepp.spring.app.model.student.document.StudentEmbedding;
+import com.grepp.spring.app.model.student.entity.Student;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentEmbeddingRepositoryTest {
+
+    @Autowired
+    StudentEmbeddingRepository studentEmbeddingRepository;
+    @Autowired
+    EmbeddingModel model;
+
+    @Test
+    public void save(){
+        Student student = new Student();
+        student.setUserId("test");
+        student.setName("test");
+        student.setMbti(MBTI.ENTP);
+        student.setMajor("backend");
+
+        studentEmbeddingRepository.save(StudentEmbedding.fromEntity(student, model));
+        assertThat(studentEmbeddingRepository.existsById("text"))
+            .isTrue();
+    }
+    @Test
+    public void findByMbti(){
+        studentEmbeddingRepository.findByMbti(MBTI.ENTP).forEach(System.out::println);
+    }
+
+}
