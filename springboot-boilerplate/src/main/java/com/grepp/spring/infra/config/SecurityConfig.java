c53a7eb69da1c0beeab06f793fa776844ed3f641
Kimoon J <jkm3093@naver.com>
2025-05-08 00:10:25 +0900

    (0425) api security url열어주기

--- springboot-boilerplate/src/main/java/com/grepp/spring/infra/config/SecurityConfig.java
+++ springboot-boilerplate/src/main/java/com/grepp/spring/infra/config/SecurityConfig.java
@@ -76,6 +76,7 @@ public class SecurityConfig {
                                   .requestMatchers(GET, "/book/list").permitAll()
                                   .requestMatchers(GET, "/api/book/list").permitAll()
                                   .requestMatchers(GET, "/api/member/exists/*").permitAll()
+                                  .requestMatchers(GET, "/api/ai/**").permitAll()
                                   .requestMatchers(GET, "/member/signup").permitAll()
                                   .requestMatchers(GET, "/member/signin").permitAll()
                                   .requestMatchers(POST, "/member/signin", "/member/signup").permitAll()
