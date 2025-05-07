320d273211f4dc96927bd77305da27888b226675
Kimoon J <jkm3093@naver.com>
2025-05-08 00:14:10 +0900

    (0425) test

--- springboot-boilerplate/src/test/java/com/grepp/spring/app/model/student/repository/StudentEmbeddingRepositoryTest.java
+++ springboot-boilerplate/src/test/java/com/grepp/spring/app/model/student/repository/StudentEmbeddingRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.grepp.spring.app.model.student.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.grepp.spring.app.model.student.code.MBTI;
 import com.grepp.spring.app.model.student.document.StudentEmbedding;
 import com.grepp.spring.app.model.student.entity.Student;
@@ -7,9 +9,6 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StudentEmbeddingRepositoryTest {
@@ -28,12 +27,33 @@ class StudentEmbeddingRepositoryTest {
         student.setMajor("backend");
 
         studentEmbeddingRepository.save(StudentEmbedding.fromEntity(student, model));
-        assertThat(studentEmbeddingRepository.existsById("text"))
+        assertThat(studentEmbeddingRepository.existsById("test"))
             .isTrue();
     }
+
     @Test
     public void findByMbti(){
-        studentEmbeddingRepository.findByMbti(MBTI.ENTP).forEach(System.out::println);
+        studentEmbeddingRepository.findByMbti(MBTI.INFP)
+                                  .forEach(System.out::println);
     }
+
+    @Test
+    public void update(){
+        StudentEmbedding embedding = studentEmbeddingRepository.findById("test").get();
+        embedding.setMbti(MBTI.INFP);
+        embedding.embed(model);
+
+        studentEmbeddingRepository.save(embedding);
+        studentEmbeddingRepository.findByMbti(MBTI.INFP)
+                                  .forEach(System.out::println);
+    }
+
+    @Test
+    public void delete(){
+        studentEmbeddingRepository.deleteById("test");
+        assertThat(studentEmbeddingRepository.existsById("test"))
+            .isFalse();
+    }
+
 
 }
