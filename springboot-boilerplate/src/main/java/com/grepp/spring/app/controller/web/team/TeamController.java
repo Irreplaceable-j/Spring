7e91fff7b774f0cd88c1598c9e25d3e4a950bd25
Kimoon J <jkm3093@naver.com>
2025-05-13 23:42:03 +0900

    (0513) TeamController

--- springboot-boilerplate/src/main/java/com/grepp/spring/app/controller/web/team/TeamController.java
+++ springboot-boilerplate/src/main/java/com/grepp/spring/app/controller/web/team/TeamController.java
@@ -3,7 +3,9 @@ package com.grepp.spring.app.controller.web.team;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +19,12 @@ public class TeamController {
     @GetMapping("{id}")
     @PreAuthorize("hasAnyAuthority('TEAM_' + #id + ':MEMBER', 'TEAM_' + #id + ':LEADER')")
     public String teamPage(
-        @PathVariable int id
+        @PathVariable int id,
+        Model model,
+        Authentication authentication
     ){
+        model.addAttribute("teamId", id);
+        model.addAttribute("username", authentication.getName());
         return "team/team";
     }
     
